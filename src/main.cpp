32651aa3819ee8fe3994d9c656f2265d1bd3b012
jeremy.dellinger.jd@gmail.com <jeremy.dellinger.jd@gmail.com>
2022-02-01 16:59:51 +0100

    Spawn new instance for each value update

--- src/main.cpp
+++ src/main.cpp
@@ -8,64 +8,132 @@
 #include <unordered_map>
 #include <utility>
 
+class jsonString;
+class JsonObject;
+
 class jsonObj {
 public:
   virtual bool parse(const std::string &s,
                      std::stack<std::unique_ptr<jsonObj>> &) = 0;
-  virtual bool parse(std::string::const_iterator &start, std::string::const_iterator end) {
-      return start == end;
+  virtual bool parse(std::string::const_iterator &start,
+                     std::string::const_iterator end,
+                     std::stack<std::unique_ptr<jsonObj>> &) {
+    return start == end;
   }
-  bool doneParsing{false};
+
+  bool newProcess(std::string::const_iterator &token,
+                  std::string::const_iterator end,
+                  std::stack<std::unique_ptr<jsonObj>> &data) {
+    if (*token == '"') {
+      auto &obj = data.emplace(std::make_unique<jsonString>());
+      return obj->parse(token, end, data);
+    }
+    if (*token == '{') {
+      auto &obj = data.emplace(std::make_unique<JsonObject>());
+      return obj->parse(token, end, data);
+    }
+    return false;
+  }
+  bool mParseComplete{false};
 };
 
 using DataType = std::stack<std::unique_ptr<jsonObj>>;
 
-class jsonString {
+class jsonString final : public jsonObj {
 public:
-  enum transition : char { start = '"', end = '"' };
+public:
+  explicit jsonString() { std::cout << "String" << std::endl; }
+
+  bool parse(const std::string &s, std::stack<std::unique_ptr<jsonObj>> &data) {
+    return process(s.begin(), s.cend(), data);
+  };
+  bool parse(std::string::const_iterator &start,
+             std::string::const_iterator end,
+             std::stack<std::unique_ptr<jsonObj>> &data) {
+    return process(start, end, data);
+  }
+
+  bool process(std::string::const_iterator token,
+               std::string::const_iterator end,
+               __attribute__((unused)) DataType &data) {
+    while (token != end) {
+      switch (mState) {
+      case State::Start:
+        if (*token != Transition::StartKeyToken) {
+          mError = "String not startin by StartToken";
+          return false;
+        }
+        mState = State::Current;
+        break;
+      case State::Current:
+        if (*token == Transition::EndKeyToken) {
+          std::cout << "read value: " << mContent << std::endl;
+          mParseComplete = true;
+          return true;
+        }
+        mContent.append(1, *token);
+        break;
+      }
+      ++token;
+    }
+    return true;
+  }
+  enum Transition : char { StartKeyToken = '"', EndKeyToken = '"' };
+  enum State : char { Start, Current };
+  std::string mError;
+  std::string mContent;
+
+  State mState{Start};
 };
 
 class jsonValue final : public jsonObj {
-  bool parse(const std::string &s, DataType &mData) override{
-      return process(s.begin(), s.cend());
+public:
+  explicit jsonValue() { std::cout << "Value" << std::endl; }
+  bool parse(const std::string &s, DataType &data) override {
+    return process(s.begin(), s.cend(), data);
   }
 
-  bool parse(std::string::const_iterator &start, std::string::const_iterator end) override {
-      return process(start, end);
+  bool parse(std::string::const_iterator &start,
+             std::string::const_iterator end, DataType &data) override {
+    return process(start, end, data);
   }
 
-  bool process(std::string::const_iterator token, std::string::const_iterator end) {
-      while (token != end) {
-          switch (mState) {
-              case State::BeginKey:
-                mState = State::CurrentKey;
-                if (*token != Transition::StartKeyToken) {
-                    mError = std::string{"String should start in new value"};
-                    return false;
-                }
-                break;
-                case State::CurrentKey:
-                 if (*token == EndKeyToken) {
-                    std::cout << "found key: " << mKey << std::endl;
-                    mState = State::Separator;
-                 } else {
-                    mKey.append(1, *token);
-                 }
-                break;
-                case State::Separator:
-                if (*token != ':') {
-                    mError = std::string{"Should get a separator"};
-                }
-                break;
-          }
-          std::cout << *token << std::endl;
-          ++token;
+  bool process(std::string::const_iterator token,
+               std::string::const_iterator end, DataType &data) {
+    while (token != end) {
+      switch (mState) {
+      case State::BeginKey:
+        mState = State::CurrentKey;
+        if (*token != Transition::StartKeyToken) {
+          mError = std::string{"String should start in new value"};
+          return false;
+        }
+        break;
+      case State::CurrentKey:
+        if (*token == EndKeyToken) {
+          std::cout << "key: " << mKey << std::endl;
+          mState = State::Separator;
+        } else {
+          mKey.append(1, *token);
+        }
+        break;
+      case State::Separator:
+        if (*token != ':') {
+          mError = std::string{"Should get a separator"};
+        }
+        mState = State::ValueStart;
+        break;
+      case State::ValueStart:
+        return newProcess(token, end, data);
       }
-      return true;
+      std::cout << *token << std::endl;
+      ++token;
+    }
+    return true;
   }
 
-  enum Transition : char { StartKeyToken = '"', EndKeyToken = '"'};
-  enum State : uint8_t { BeginKey, CurrentKey, Separator, Value };
+  enum Transition : char { StartKeyToken = '"', EndKeyToken = '"' };
+  enum State : uint8_t { BeginKey, CurrentKey, Separator, ValueStart };
   State mState{State::BeginKey};
   std::string mKey;
   std::string mError;
@@ -73,9 +141,21 @@ class jsonValue final : public jsonObj {
 
 class JsonObject final : public jsonObj {
 public:
+  explicit JsonObject() { std::cout << "String" << std::endl; }
+
   enum Token : char { Start = '{', StringStart = '\"', End = '}' };
-  bool parse(const std::string &s, DataType &mData) override {
-    for (auto token = s.cbegin(); token != s.cend(); ++token) {
+  bool parse(std::string::const_iterator &start,
+             std::string::const_iterator end, DataType &data) override {
+    return process(start, end, data);
+  }
+
+  bool parse(const std::string &s, DataType &data) override {
+    return process(s.begin(), s.cend(), data);
+  }
+
+  bool process(std::string::const_iterator token,
+               std::string::const_iterator end, DataType &data) {
+    while (token != end) {
       switch (mState) {
       case State::Begin:
         if (*token != Token::Start) {
@@ -85,11 +165,11 @@ public:
         break;
       case State::StartValue:
         if (*token == Token::StringStart) {
-          auto& obj = mData.emplace(std::make_unique<jsonValue>());
-          return obj->parse(token, s.cend());
+          auto &obj = data.emplace(std::make_unique<jsonValue>());
           mState = State::CurrentValue;
+          return obj->parse(token, end, data);
         } else if (*token == Token::End) {
-          doneParsing = true;
+          mParseComplete = true;
         } else {
           mError = std::string{"No StartVale or End token available"};
           return false;
@@ -100,6 +180,7 @@ public:
       } break;
       }
       std::cout << *token << std::endl;
+      ++token;
     }
     return true;
   }
@@ -119,6 +200,9 @@ public:
     } else {
       auto &obj = mData.top();
       obj->parse(line, mData);
+      if (obj->mParseComplete == true) {
+        mData.pop();
+      }
     }
   }
 
